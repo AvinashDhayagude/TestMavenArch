36ae3fee619022cd7798cebbee71d3422972472a
AvinashDhayagude <avi.dhaygude@gmail.com>
2015-06-26 10:16:10 +0530

    Update App.java

--- src/main/java/org/javabrains/avinash/App.java
+++ src/main/java/org/javabrains/avinash/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to the world of GIT MAVEN AND Jenkins!" );
+        System.out.println( "Welcome to the world of GIT MAVEN AND Jenkins and now GErrit!" );
     }
 }
